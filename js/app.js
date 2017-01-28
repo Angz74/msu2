a7f22d7436e0ddcc0b77e6813ad3aa407188392d
Angela Zhang <ang.zhang74@gmail.com>
2017-01-28 16:04:57 -0500

    update data

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
-var ordinalScale = d3.scaleOrdinal()
-	.domain(['cold', 'cool','ok', 'nice', 'warm'])
-	.range (['blue','lightblue','white','yellow','green']);
+d3.json("js/data.json", function (data){
 
-console.log(ordinalScale('cool'));
-console.log(ordinalScale('warm'));
-console.log(ordinalScale('ok'));
+	var min = d3.min(data, function(d){
+		return d.age;
+	});
+
+	
+
+	var max = d3.max(data, function(d){
+		return d.age;
+	});
+
+	var ages = d3.set(data, function(d){
+		return d.age;
+		// if return d.name;, it will return as a name
+	})
+	console.log(ages.values());
+})
+
+http://localhost:8080/
